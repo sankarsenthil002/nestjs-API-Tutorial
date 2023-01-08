32d73d6aac4d5f65e3b547e31eb810a827f0fb88
sankarsenthil002 <121735185+sankarsenthil002@users.noreply.github.com>
2023-01-07 22:25:05 -0500

    first

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello Nest JS learning!"', () => {
+      expect(appController.getHello()).toBe('Hello Nest JS learning!');
+    });
+  });
+});
